687b861a16cf65e1e91355b11d4d96632383211f
dvpv <david.pava@protonmail.com>
2023-05-29 15:01:56 +0300

    Added card deletion and edit state

--- lib/src/presentation/components/card_list_tile.dart
+++ lib/src/presentation/components/card_list_tile.dart
@@ -5,6 +5,7 @@ class CardListTile extends StatefulWidget {
   const CardListTile({
     required this.onFrontChanged,
     required this.onBackChanged,
+    required this.onDelete,
     this.front = '',
     this.back = '',
     super.key,
@@ -14,6 +15,7 @@ class CardListTile extends StatefulWidget {
   final String back;
   final void Function(String change) onFrontChanged;
   final void Function(String change) onBackChanged;
+  final VoidCallback onDelete;
 
   @override
   State<CardListTile> createState() => _CardListTileState();
@@ -22,6 +24,7 @@ class CardListTile extends StatefulWidget {
 class _CardListTileState extends State<CardListTile> {
   final TextEditingController _frontController = TextEditingController();
   final TextEditingController _backController = TextEditingController();
+  bool _editing = false;
 
   @override
   void initState() {
@@ -32,78 +35,128 @@ class _CardListTileState extends State<CardListTile> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.8,
-      height: 250,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: AppColors.blue300,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16),
-                  topRight: Radius.circular(16),
+    return GestureDetector(
+      onTap: !_editing
+          ? () {
+              setState(() {
+                _editing = true;
+              });
+            }
+          : null,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: _editing ? 300 : 250,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: AppColors.blue300,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                  ),
+                ),
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return Center(
+                      child: SizedBox(
+                        width: constraints.maxWidth * 0.8,
+                        height: constraints.maxHeight * 0.8,
+                        child: TextField(
+                          maxLines: null,
+                          enabled: _editing,
+                          keyboardType: TextInputType.multiline,
+                          textAlign: TextAlign.center,
+                          controller: _frontController,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Add your front text here.',
+                          ),
+                          onChanged: widget.onFrontChanged,
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
-              child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  return Center(
-                    child: SizedBox(
-                      width: constraints.maxWidth * 0.8,
-                      height: constraints.maxHeight * 0.8,
-                      child: TextField(
-                        maxLines: null,
-                        keyboardType: TextInputType.multiline,
-                        textAlign: TextAlign.center,
-                        controller: _frontController,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Add your front text here.',
-                        ),
-                        onChanged: widget.onFrontChanged,
-                      ),
-                    ),
-                  );
-                },
-              ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: AppColors.blue500,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(16),
-                  bottomRight: Radius.circular(16),
+            Expanded(
+              flex: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColors.blue500,
+                  borderRadius: _editing
+                      ? null
+                      : const BorderRadius.only(
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                        ),
+                ),
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return Center(
+                      child: SizedBox(
+                        width: constraints.maxWidth * 0.8,
+                        height: constraints.maxHeight * 0.8,
+                        child: TextField(
+                          maxLines: null,
+                          enabled: _editing,
+                          keyboardType: TextInputType.multiline,
+                          textAlign: TextAlign.center,
+                          controller: _backController,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Add your back text here.',
+                          ),
+                          onChanged: widget.onBackChanged,
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
-              child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  return Center(
-                    child: SizedBox(
-                      width: constraints.maxWidth * 0.8,
-                      height: constraints.maxHeight * 0.8,
-                      child: TextField(
-                        maxLines: null,
-                        keyboardType: TextInputType.multiline,
-                        textAlign: TextAlign.center,
-                        controller: _backController,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Add your back text here.',
-                        ),
-                        onChanged: widget.onBackChanged,
-                      ),
-                    ),
-                  );
-                },
-              ),
             ),
-          ),
-        ],
+            if (_editing)
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: AppColors.blue600,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            widget.onDelete();
+                          });
+                        },
+                        child: const Text('Delete'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _editing = false;
+                          });
+                        },
+                        child: const Text('Save'),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            else
+              Container(),
+          ],
+        ),
       ),
     );
   }
--- lib/src/presentation/deck/create_deck_page.dart
+++ lib/src/presentation/deck/create_deck_page.dart
@@ -20,11 +20,38 @@ class _CreateDeckPageState extends State<CreateDeckPage> {
       _cards.add(
         Flashcard(
           id: const Uuid().v1(),
-          front: 'Front face',
-          back: 'Back face',
+          front: 'Card ${_cards.length + 1}',
+          back: '',
         ),
       );
     });
+  }
+
+  Future<void> _deleteFlashcard(BuildContext context, Flashcard card) async {
+    await showDialog<void>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Delete card?'),
+        content: const Text('Are you sure you want to delete this card?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _cards.removeWhere((Flashcard x) => x == card);
+              });
+              Navigator.of(context).pop();
+            },
+            child: const Text('Delete'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -59,19 +86,25 @@ class _CreateDeckPageState extends State<CreateDeckPage> {
           itemBuilder: (BuildContext context, int index) {
             return Padding(
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              child: CardListTile(
-                front: 'Front $index',
-                back: 'Back $index',
-                onBackChanged: (String change) {
-                  setState(() {
-                    _cards[index] = _cards[index].copyWith(back: change);
-                  });
-                },
-                onFrontChanged: (String change) {
-                  setState(() {
-                    _cards[index] = _cards[index].copyWith(front: change);
-                  });
-                },
+              child: Center(
+                child: CardListTile(
+                  key: ValueKey<String>(_cards[index].id),
+                  front: _cards[index].front,
+                  back: _cards[index].back,
+                  onBackChanged: (String change) {
+                    setState(() {
+                      _cards[index] = _cards[index].copyWith(back: change);
+                    });
+                  },
+                  onFrontChanged: (String change) {
+                    setState(() {
+                      _cards[index] = _cards[index].copyWith(front: change);
+                    });
+                  },
+                  onDelete: () {
+                    _deleteFlashcard(context, _cards[index]);
+                  },
+                ),
               ),
             );
           },
