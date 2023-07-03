b6468fe412247896e6816336bf3fea198d2e0b66
dvpv <david.pava@protonmail.com>
2023-07-03 17:17:31 +0300

    Added basic pay page
    Added a basic pay page.

--- lib/src/presentation/pay/pay_page.dart
+++ lib/src/presentation/pay/pay_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class PayPage extends StatelessWidget {
   const PayPage({super.key});
@@ -18,8 +19,35 @@ class PayPage extends StatelessWidget {
         elevation: 0,
         leading: const BackButton(color: Colors.black),
       ),
-      body: const Center(
-        child: Text('Pay'),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: CardFormField(
+                style: CardFormStyle(
+                  backgroundColor: Theme.of(context).colorScheme.surface,
+                  textColor: Theme.of(context).colorScheme.onSurface,
+                  fontSize: 18,
+                  cursorColor: Theme.of(context).colorScheme.primary,
+                  placeholderColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+                ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Pay',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
